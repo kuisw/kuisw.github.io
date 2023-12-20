56a550a70133539a1865ac77a9b7aa40bff26a6e
kuisw <154001914+kuisw@users.noreply.github.com>
2023-12-20 22:51:05 +0800

    update post

--- src/js/PostList.js
+++ src/js/PostList.js
@@ -4,24 +4,49 @@ var PostList = [
         "title": "测试",
         "Introduction": "测试测试测试测试测试测试",
         "tags": [
-            "vue",
-            "js"
+            "#vue",
+            "#js"
         ]
     }, {
         "id": 2,
         "title": "测试2",
         "Introduction": "测试2测试2测试2测试2测试2测试2测试2测试2",
         "tags": [
-            "c++",
-            "测试"
+            "#c++",
+            "#测试"
         ]
     }
 ]
 
-for(var i = 0; i < PostList.length; i++) {
-    document.write("<tr>");
-    for(var j = 0; j < PostList[i].length; j++) {
-        document.write("<td>" + PostList[i][j] + "</td>");
+window.onload = function () {
+    var postListContainer = document.getElementById("postList");
+    for (var i = 0; i < PostList.length; i++) {
+        var post = PostList[i];
+        var liElement = document.createElement("li");
+        liElement.classList.add("post")
+
+        // 创建标题元素
+        var titleElement = document.createElement("h3");
+        titleElement.innerText = post.title;
+        liElement.appendChild(titleElement);
+
+        // 创建介绍元素
+        var introElement = document.createElement("p");
+        introElement.innerText = post.Introduction;
+        introElement.classList.add("post-intro"); // 添加不同于title的class
+        liElement.appendChild(introElement);
+
+        // 创建tags的ul元素
+        var tagsElement = document.createElement("ul"); 
+        tagsElement.classList.add("tags-container");
+        for (var j = 0; j < post.tags.length; j++) {
+            var tag = post.tags[j];
+            var tagElement = document.createElement("li"); // 创建tag的li元素
+            tagElement.innerText = tag;
+            tagsElement.appendChild(tagElement); // 将tag的li元素添加到tags的ul元素中
+        }
+        liElement.appendChild(tagsElement); // 将tags的ul元素添加到li元素中
+
+        postListContainer.appendChild(liElement);
     }
-    document.write("</tr>");
 }
